f2c54f8b228c254c13bafb131fd69cbd33282bea
ABHINAV RAJ <92502850+probably-ABHINAV@users.noreply.github.com>
2025-07-23 15:58:05 +0530

    Updated

--- lib/supabase-server.ts
+++
@@ -1,7 +0,0 @@
-
-import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
-import { cookies } from 'next/headers'
-
-export const createServerClient = () => {
-  return createServerComponentClient({ cookies })
-}
