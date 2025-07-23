e08b87d7c1dece4f2a8565168fb340d4523320e3
ABHINAV RAJ <92502850+probably-ABHINAV@users.noreply.github.com>
2025-07-23 23:05:31 +0530

    Updated

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,19 +1,13 @@
-'use client'
+"use client"
 
-import * as React from 'react'
-import {
-  ThemeProvider as NextThemesProvider,
-  type ThemeProviderProps,
-} from 'next-themes'
+import * as React from "react"
+import { ThemeProvider as NextThemesProvider, type ThemeProviderProps } from "next-themes"
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return (
     <NextThemesProvider
-      attribute="class"
-      defaultTheme="system"
       enableSystem
       disableTransitionOnChange
-      suppressHydrationWarning
       {...props}
     >
       {children}
