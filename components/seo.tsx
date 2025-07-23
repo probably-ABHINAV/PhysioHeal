c8254f44d13774db2280e8f815e39fa2eaa64316
ABHINAV RAJ <92502850+probably-ABHINAV@users.noreply.github.com>
2025-07-23 12:45:59 +0530

    Updated

--- components/seo.tsx
+++ components/seo.tsx
@@ -1,24 +1,19 @@
-// components/seo.tsx
-
 "use client"
 
 import { FC } from "react"
 import { NextSeo, NextSeoProps } from "next-seo"
-import { ModifiedJsonLd } from "./jsonld"
 
 type SeoProps = NextSeoProps & {
-  schema?: Record<string, any>
-  canonical?: string // ✅ ADD THIS LINE
+  canonical?: string
 }
 
-export const SEO: FC<SeoProps> = ({ schema, canonical, ...props }) => {
+export const Seo: FC<SeoProps> = ({ canonical, ...props }) => {
   return (
     <>
       <NextSeo
         {...props}
         canonical={canonical || `https://physioheal.com${props.canonical || ""}`}
       />
-      {schema && <ModifiedJsonLd item={schema} />}
     </>
   )
 }
